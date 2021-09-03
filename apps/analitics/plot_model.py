0d937dfeec2aebe85f7fa79ece25711340d81546
Jaime Andrés Molina <71019961+JaimeAndres-M@users.noreply.github.com>
2021-09-03 09:37:30 -0500

    correcion variables plot_model

--- apps/analitics/plot_model.py
+++ apps/analitics/plot_model.py
@@ -13,20 +13,21 @@ from sklearn.cluster import KMeans
 # dataset
 df_modelo1 = pd.read_csv('SELECT * FROM df_modelo')
 
-df_modelo1[["TP15_1_OCU", "TP19_EE_1", "TP19_ACU_1", "TP19_ALC_1", "TP19_GAS_1", "TP19_RECB1", "TP19_INTE1", "TP51SUPERI",
-            "TP51POSTGR", "TP51_13_ED", "TP15_2_OCU", "TP19_EE_2", "TP19_ACU_2", "TP19_ALC_2", "TP19_GAS_2", "TP19_RECB2",
-            "TP19_INTE2", "TP51PRIMAR", "TP51SECUND", "icbf_ben", "salud_pi", "salud_i", "salud_a", "malnutricion_2020",
-            "matricula_com_pi", "matricula_com_i", "matricula_com_a"]] = df_modelo1[["TP15_1_OCU", "TP19_EE_1", "TP19_ACU_1", "TP19_ALC_1", "TP19_GAS_1", "TP19_RECB1", "TP19_INTE1", "TP51SUPERI",
-                                                                                     "TP51POSTGR", "TP51_13_ED", "TP15_2_OCU", "TP19_EE_2", "TP19_ACU_2", "TP19_ALC_2", "TP19_GAS_2", "TP19_RECB2",
-                                                                                     "TP19_INTE2", "TP51PRIMAR", "TP51SECUND", "icbf_ben", "salud_pi", "salud_i", "salud_a", "malnutricion_2020",
+df_modelo1[["tp15_1_ocu", "tp19_ee_1", "tp19_acu_1", "tp19_alc_1", "tp19_gas_1", "tp19_recb1", "tp19_inte1", "tp51superi",
+            "tp51postgr", "tp51_13_ed", "tp15_2_ocu", "tp19_ee_2", "tp19_acu_2", "tp19_alc_2", "tp19_gas_2", "tp19_recb2",
+            "tp19_inte2", "tp51primar", "tp51secund", "icbf_ben", "salud_pi", "salud_i", "salud_a", "malnutricion_2020",
+            "matricula_com_pi", "matricula_com_i", "matricula_com_a"]] = df_modelo1[["tp15_1_ocu", "tp19_ee_1", "tp19_acu_1", "tp19_alc_1", "tp19_gas_1", "tp19_recb1", "tp19_inte1", "tp51superi",
+                                                                                     "tp51postgr", "tp51_13_ed", "tp15_2_ocu", "tp19_ee_2", "tp19_acu_2", "tp19_alc_2", "tp19_gas_2", "tp19_recb2",
+                                                                                     "tp19_inte2", "tp51primar", "tp51secund", "icbf_ben", "salud_pi", "salud_i", "salud_a", "malnutricion_2020",
                                                                                      "matricula_com_pi", "matricula_com_i", "matricula_com_a"]].astype("float64")
+
 
 df_modelo1[["poblacion_2020"]] = df_modelo1[["poblacion_2020"]].astype("int")
 
 # PCA
 df = df_modelo1
 X = df_modelo1.drop(
-    columns=["CD_LC_CM", "NMB_LC_CM_x", "comuna", "poblacion_2020"])
+    columns=["cd_lc_cm", "nmb_lc_cm_x", "comuna", "poblacion_2020"])
 features = X.columns.to_list()
 
 pca = PCA(n_components=2)  # Number of components
